0b35f3033500bd4edad5813b450442fa5627cc84
yfoo <yfoo@protonmail.com>
2024-04-29 23:19:31 -0700

    change to 1.0

--- src/MainScript.java
+++ src/MainScript.java
@@ -12,7 +12,7 @@ import org.osbot.rs07.script.ScriptManifest;
 
 import static Util.PickpocketUtil.PICKPOCKET;
 
-@ScriptManifest(author = "yfoo", name = "[dbg1] Mark & Steal", info = "Mark target NPC to have this bot to pickpocket them!", version = 0.9, logo = "")
+@ScriptManifest(author = "yfoo", name = "Mark & Steal", info = "Mark target NPC to have this bot to pickpocket them!", version = 1.0, logo = "")
 public class MainScript extends Script {
 
     ScriptPaint scriptPaint;
